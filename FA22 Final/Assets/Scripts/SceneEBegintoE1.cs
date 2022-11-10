85078361c4274586e542a93bd62ef629dfe938f2
babybabel <schrs973@newschool.edu>
2022-11-09 21:25:49 -0500

    WORKING SCENE TRANSITION FROM BEGIN TO E1

--- FA22 Final/Assets/Scripts/SceneEBegintoE1.cs
+++ FA22 Final/Assets/Scripts/SceneEBegintoE1.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneEBegintoE1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadSceneE1()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("SceneE1");
     }
 }
