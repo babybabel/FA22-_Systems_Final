b0575f6eaab6159d15bdbd2e1cb7561078875c4a
babybabel <schrs973@newschool.edu>
2022-11-11 02:12:03 -0500

    more scene transitions

---
+++ FA22 Final/Assets/Scripts/SceneE1toE2.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneE1toE2 : MonoBehaviour
+{
+    public void LoadSceneE2()
+    {
+        SceneManager.LoadScene("SceneE2");
+    }
+}
---
+++ FA22 Final/Assets/Scripts/SceneE2toE3.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneE2toE3 : MonoBehaviour
+{
+    public void LoadSceneE3()
+    {
+        SceneManager.LoadScene("SceneE3");
+    }
+}
