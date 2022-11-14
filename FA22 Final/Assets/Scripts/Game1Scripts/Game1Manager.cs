118dd230ce8f3f666d99465d3e137097a5ff46ae
babybabel <schrs973@newschool.edu>
2022-11-14 18:10:24 -0500

    visual game success and fail feedback

--- FA22 Final/Assets/Scripts/Game1Scripts/Game1Manager.cs
+++ FA22 Final/Assets/Scripts/Game1Scripts/Game1Manager.cs
@@ -7,11 +7,15 @@ public class Game1Manager : MonoBehaviour
 {
     public FunnelTrigger funnelTrigger;
     public EdgeTrigger edgeTrigger;
+    public Animator failAnim;
+    public Animator successAnim;
 
     private void Start()
     {
         funnelTrigger = GameObject.Find("FunnelTrigger").GetComponent<FunnelTrigger>();
         edgeTrigger = GameObject.Find("EdgeTrigger").GetComponent<EdgeTrigger>();
+        failAnim = GetComponent<Animator>();
+        successAnim = GetComponent<Animator>();
     }
 
     public void Game1Results()
@@ -19,10 +23,12 @@ public class Game1Manager : MonoBehaviour
         if (funnelTrigger.Game1Fail == false || edgeTrigger.Game1Fail == false)
         {
             Debug.Log("You Passed Game 1!");
+            successAnim.Play("Checkanim");
         }
-        else
+        if (funnelTrigger.Game1Fail == true || edgeTrigger.Game1Fail == true)
         {
             Debug.Log("You Failed Game 1!");
+            failAnim.Play("Xanim");
         }
     }
     
