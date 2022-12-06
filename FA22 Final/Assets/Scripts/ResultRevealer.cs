7c445f325cdd3cc2f4bcf96b50b0c19a9b08eca5
babybabel <schrs973@newschool.edu>
2022-12-06 15:34:36 -0500

    microadjustments

--- FA22 Final/Assets/Scripts/ResultRevealer.cs
+++ FA22 Final/Assets/Scripts/ResultRevealer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,33 @@ public class ResultRevealer : MonoBehaviour
 {
 
     public GameObject[] result;
-    
-    
 
+    void Start()
+    {
+        if (Scoring.totalScore == 0)
+        {
+            result[0].SetActive(true);
+        } else if (Scoring.totalScore == 1)
+        {
+            result[1].SetActive(true);
+        } else if (Scoring.totalScore == 2)
+        {
+            result[2].SetActive(true);
+        } else if (Scoring.totalScore == 3)
+        {
+            result[3].SetActive(true);
+        } else if (Scoring.totalScore == 4)
+        {
+            result[4].SetActive(true);
+        } else if (Scoring.totalScore == 5)
+        {
+            result[5].SetActive(true);
+        } else if (Scoring.totalScore == 6)
+        {
+            result[6].SetActive(true);
+        } else if (Scoring.totalScore == 7)
+        {
+            result[7].SetActive(true);
+        }
+    }
 }
